50cf49304f61dca56ac20d46a2868a81663f1f8d
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-05-27 11:27:55 -0300

    Update jogodesenvolvido.java

--- jogodesenvolvido.java
+++ jogodesenvolvido.java
@@ -1,8 +1,21 @@
-public class jogodesenvolvido{
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jogodesenvolvido;
+
+/**
+ *
+ * @author alunolab10
+ */
+public class Jogodesenvolvido {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
-        InOut.leString("Insira uma nova arma: (faca)");
-        InOut.leString("Insira um novo personagem: (Mago)");
+        InOut.leInt("Insira uma nova arma: (faca)");
+        InOut.leInt("Insira um novo personagem: (Mago)");
     }
     
 }
