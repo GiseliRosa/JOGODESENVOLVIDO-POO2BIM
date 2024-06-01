dd10ad7c100c426d54d2003fcb4ef6081a574ed7
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-06-01 00:24:20 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -8,7 +8,18 @@ package jogodesenvolvido;
     Arma Arma_IF;
     int vida;
     String nome;
-  
+
+  public Personagem(String nome, Arma Arma_IF) {
+        this.nome = nome;
+        this.Arma = Arma_IF;
+    }
+
+
+  }
+
+  public String getnome() {
+    return nome;
+}
     public abstract void desenhar();
   
     public abstract void falar();
@@ -25,4 +36,22 @@ package jogodesenvolvido;
     public void usarArma() {
        
     }
-  }
+     public void setArma(Arma_IF) {
+         this.Arma_IF = Arma_IF;
+          }
+
+    
+ public void correr() {
+        System.out.println(nome + " está correndo para se proteger!");
+    }
+
+    public void voar() {
+        System.out.println(nome + " está voando.");
+    }
+
+ public void atacar() {
+        Arma.usarArma();
+        System.out.println(nome + " atacou causando " + Arma.getDano() + " de dano.");
+    }
+  
+  
